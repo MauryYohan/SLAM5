54ad820108c233fa793bffeacd32e79695746119
MauryYohan <mauryyohan1@gmail.com>
2017-03-24 10:56:32 +0100

    Commit first !

---
+++ StPaulMVC/model.php
@@ -0,0 +1,12 @@
+<?php
+$bdd = new PDO('mysql:host=localhost;dbname=bdstpaul;charset=utf8', 'root');
+$sejours = $bdd->query('select * from sejour order by sejno');
+
+foreach ($sejours as $sejour) {
+	echo $sejour['SEJINTITULE'];
+	echo $sejour['SEJMONTANTMBI'].'€';
+	echo 'A partir du '.$sejour['SEJDTEDEB'];
+	echo $sejour['SEJDUREE'].' nuits';
+}
+
+?>
